256f88e6c55247788b498a3128ef28b1a1b3b133
Arun Giri <AGiri09@my.fisk.edu>
2022-04-08 23:18:55 -0500

    checking for dinesh

--- lunch.js
+++ lunch.js
@@ -15,24 +15,7 @@ function updatedata(data) {
     
     let allCardsDom = '';
     data.forEach((value)=>{
-        // console.log(value.Type);
-    //     if (evalue.titl === "fries"){
-    //         const cardTemplate = 
-    //         `<div class="col">
-    //             <div class="card">
-    //                 <a href=""><img src="${value.image}"
-    //                     class="card-img-top" alt="..."></a>
-    //                 <div class="card-body">
-    //                     <h5 class="card-title">${value.title}</h5>
-    //                     <p class="card-text">${value.description}</p>
-    //                 </div>
-    //             </div>
-    //         </div>`;
-    //      allCardsDom +=cardTemplate;
-    //     }
-    // realContainer.innerHTML = allCardsDom;
-
-    //     });
+        console.log(value.Type);
         const cardTemplate = 
         // console.log(value.Type);
             `<div class="col">
@@ -51,6 +34,25 @@ function updatedata(data) {
     realContainer.innerHTML = allCardsDom;
 }
 
-function foodfinder(data){
-    console.log(55);
-}
+
+
+
+
+
+// if (evalue.titl === "fries"){
+    //         const cardTemplate = 
+    //         `<div class="col">
+    //             <div class="card">
+    //                 <a href=""><img src="${value.image}"
+    //                     class="card-img-top" alt="..."></a>
+    //                 <div class="card-body">
+    //                     <h5 class="card-title">${value.title}</h5>
+    //                     <p class="card-text">${value.description}</p>
+    //                 </div>
+    //             </div>
+    //         </div>`;
+    //      allCardsDom +=cardTemplate;
+    //     }
+    // realContainer.innerHTML = allCardsDom;
+
+    //     });
